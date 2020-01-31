bf26f7a685bdf4c6892f569a4a0fac73fa0c9cb8
Zack Devine <zdevine@me.com>
2020-01-30 22:07:02 -0500

    Update user object to allow access tokens to be stored

--- app/User.php
+++ app/User.php
@@ -8,14 +8,8 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
-    protected $fillable = [ 'username', 'userid', 'discrim', 'avatar' ];
+    protected $fillable = [ 'username', 'userid', 'discrim', 'avatar', 'access_token' ];
     protected $hidden = [ 'access_token', 'remember_token' ];
     
-    public function isStaff() { return api_IsStaff($this->userid); }
-    public function getDisplayName($badge = false, $color = 'primary') { return "{$this->username}#{$this->discrim}"; }
-    public function getStaffBadge($color = 'primary')
-    {
-        if (!$this->isStaff()) return "";
-        return "<span class='tag is-$color'>Rythm Staff</span>";
-    }
+    public function getDisplayName() { return "{$this->username}#{$this->discrim}"; }
 }
